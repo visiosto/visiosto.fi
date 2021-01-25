2d8d67ef99a792a38e48d6c72bf0f0599183c5d8
Antti Kivi <antti.kivi@visiosto.fi>
2021-01-25 23:10:04 +0200

    Add initial colours

---
+++ src/theme.js
@@ -0,0 +1,12 @@
+// Coypright (c) 2021 Visiosto oy
+// Licensed under the MIT License
+
+const colors = {
+  blue: "#91d7f2",
+  turquoise: "#5196a6",
+  white: "#f2f0eb",
+  orange: "#f2955e",
+  red: "#eb5952"
+};
+
+export {colors};
