2f2f7912086e958831e506a05bfc761c4063453d
Antti Kivi <antti.kivi@visiosto.fi>
2021-01-25 23:09:45 +0200

    Include styled components and Airbnb javascript style

---
+++ gatsby-config.js
@@ -0,0 +1,19 @@
+// Copyright (c) 2021 Visiosto oy
+// Licensed under the MIT License
+
+module.exports = {
+  siteMetadata: {
+    siteUrl: "https://visiosto.fi",
+    title: "Visiosto",
+    description: "Sinun visiosi - linssimme läpi",
+    twitterAuthor: "@visiosto_oy"
+  },
+  plugins: [
+    {
+      resolve: "gatsby-plugin-styled-components",
+      options: {
+        // Add any options here
+      },
+    }
+  ]
+};
