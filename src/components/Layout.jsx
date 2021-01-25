40b6ec471083a43b92877cbfa2d911aa128cfcb6
Antti Kivi <antti.kivi@visiosto.fi>
2021-01-26 00:29:56 +0200

    Add dummy component for the site layout

---
+++ src/components/Layout.jsx
@@ -0,0 +1,14 @@
+// Copyright (c) 2021 Visiosto oy
+// Licensed under the MIT License
+
+import React from "react";
+
+import Head from "./Head";
+
+export default Layout = props => {
+  return (
+    <>
+      <Head />
+    </>
+  );
+};
