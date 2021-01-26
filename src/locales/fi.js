479be3b4ec9afe31d1ea5bd5fbbf1b59c1b96e26
Antti Kivi <antti.kivi@visiosto.fi>
2021-01-26 09:30:25 +0200

    Add base translation files for Finnish

---
+++ src/locales/fi.js
@@ -0,0 +1,20 @@
+// Copyright (c) 2021 Visiosto oy
+// Licensed under the MIT License
+
+import { defineMessages } from 'react-intl';
+
+import footerMessages from './fi/footer';
+import headerMessages from './fi/header';
+import indexMessages from './fi/index';
+import metaMessages from './fi/meta';
+import navigationMessages from './fi/navigation';
+
+export const messages = {
+  ...footerMessages,
+  ...headerMessages,
+  ...indexMessages,
+  ...metaMessages,
+  ...navigationMessages,
+};
+
+export const lang = defineMessages(messages);
