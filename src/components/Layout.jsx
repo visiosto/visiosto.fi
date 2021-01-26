02aef804d5546507c57ef9ff18fe581a6bde9258
Antti Kivi <antti.kivi@visiosto.fi>
2021-01-26 09:31:17 +0200

    Create global style for site and use dark mode as state

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,14 +1,51 @@
 // Copyright (c) 2021 Visiosto oy
 // Licensed under the MIT License
 
-import React from 'react';
+import React, { useState } from 'react';
+import { ThemeProvider, createGlobalStyle } from 'styled-components';
 
 import Head from './Head';
 
-export default Layout = (props) => {
+import theme from '../theme';
+
+const GlobalStyle = createGlobalStyle`
+  *, *::before, *::after {
+    box-sizing: inherit;
+  }
+
+  html {
+    box-sizing: border-box;
+  }
+
+  body {
+    background: ${(props) =>
+      props.colorScheme === 'dark' ? props.theme.colors.black : props.theme.colors.white};
+    font-family: ${(props) => props.theme.fonts.main};
+  }
+`;
+
+const Layout = (props) => {
+  const [colorScheme, setColorScheme] = useState(
+    window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches
+      ? 'dark'
+      : 'light',
+  );
+
+  window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', (e) => {
+    setColorScheme(e.matches ? 'dark' : 'light');
+  });
+
   return (
     <>
-      <Head />
+      <GlobalStyle colorScheme={colorScheme} />
+      <Head {...props} />
+      {props.children}
     </>
   );
 };
+
+export default (props) => (
+  <ThemeProvider theme={theme}>
+    <Layout {...props} />
+  </ThemeProvider>
+);
