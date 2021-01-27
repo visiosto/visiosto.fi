bea76cb0a3366bb1e956a7efbb333be9551a7ebb
Antti Kivi <antti.kivi@visiosto.fi>
2021-01-27 11:59:11 +0200

    Change the Finnish translation of the index cover text

--- src/locales/fi/index.js
+++ src/locales/fi/index.js
@@ -5,7 +5,7 @@ export default {
   indexTitle: 'Etusivu',
   indexCoverTitle: 'Sinun visiosi – linssimme läpi',
   indexCoverContent:
-    'Visio on toiminnan suunta, joten miksi tyytyä toteutuksessa keskivertoon sanahelinään, kun todellisuus muovautuu tekojen kautta. Me Visiostossa uskomme rohkeaan ja laadukkaaseen tekemiseen ja persoonalliseen vuorovaikutukseen. Pilvilinnojen maalauksen aika on ohi: me rakennamme sen, mitä toivot. Taitamme sinun visiosi – linssimme läpi.',
+    'Visio on toiminnan veturi, joten miksi tyytyä toteutuksessa keskivertoon sanahelinään, kun todellisuus muovautuu tekojen kautta. Me Visiostossa uskomme rohkeaan ja laadukkaaseen tekemiseen ja persoonalliseen vuorovaikutukseen. Pilvilinnojen maalauksen aika on ohi: me rakennamme sen, mitä toivot. Taitamme sinun visiosi – linssimme läpi.',
   indexStoryTitle: 'Tarinamme',
   indexStoryContent:
     'Visiosto on kolmen ystävyksen projekti, joka lähti liikkeelle kyllästymisestä visuaalisesti kammottaviin graafisiin ilmeisiin sekä kasvottomaan asiakastyöhön. Olemme nykyajan viestintätoimisto, joka luo arvoa sekä asiakkaalle että yhteiskunnalle. Olemme olemassa, jotta viestisi koskettaa vastaanottajan sydäntä.',
