c5fbc6b4ba5ceab6e26f58e1f14e7c80226b78e8
Antti Kivi <antti.kivi@visiosto.fi>
2021-01-30 18:35:06 +0200

    Fix the images by adding the relative positioning back

--- src/components/Cover.jsx
+++ src/components/Cover.jsx
@@ -7,6 +7,7 @@ import styled from 'styled-components';
 export default (props) => {
   const Div = styled.div`
     overflow: hidden;
+    position: relative;
     min-width: 100%;
     min-height: 100%;
     margin: 2em 0;
