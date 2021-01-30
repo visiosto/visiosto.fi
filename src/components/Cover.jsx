7a6b18495418d4166b4666c83223aaf4f249f0dc
Antti Kivi <antti.kivi@visiosto.fi>
2021-01-30 18:28:34 +0200

    Remove the unnecessary styles from the cover

--- src/components/Cover.jsx
+++ src/components/Cover.jsx
@@ -7,8 +7,6 @@ import styled from 'styled-components';
 export default (props) => {
   const Div = styled.div`
     overflow: hidden;
-    display: inline-block;
-    position: relative;
     min-width: 100%;
     min-height: 100%;
     margin: 2em 0;
