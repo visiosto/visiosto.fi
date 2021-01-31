4d2eb07b925b66b301a11806e07b37644f101885
Antti Kivi <antti.kivi@visiosto.fi>
2021-01-31 16:53:17 +0200

    Fix the exports of the browser API

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,15 +1,15 @@
 // Copyright (c) 2021 Visiosto oy
 // Licensed under the MIT License
 
+// Based on code by Chase Ohlson.
+// Original code is available at https://github.com/brohlson/gatsby-plugin-anchor-links.
+
 import 'normalize.css';
 
 import checkHash from './src/utils/anchor-link/checkHash';
 import scroller from './src/utils/anchor-link/scroller';
 
-// Based on code by Chase Ohlson.
-// Original code is available at https://github.com/brohlson/gatsby-plugin-anchor-links.
-
-exports.onRouteUpdate = ({ location }, { offset = 0, duration = 1000 }) => {
+const onRouteUpdate = ({ location }, { offset = 0, duration = 1000 }) => {
   const isBrowser = typeof window !== 'undefined';
 
   const windowHash = isBrowser ? window.visiostoScrollHash : undefined;
@@ -29,3 +29,5 @@ exports.onRouteUpdate = ({ location }, { offset = 0, duration = 1000 }) => {
     window.visiostoScrollHash = undefined;
   }
 };
+
+export default { onRouteUpdate };
