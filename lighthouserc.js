d3d7ce6ec8e95fea78b023695b817414de850264
Antti Kivi <antti.kivi@visiosto.fi>
2021-01-31 20:11:52 +0200

    Add Lighthouse configuration

---
+++ lighthouserc.js
@@ -0,0 +1,12 @@
+// Copyright (c) 2021 Visiosto oy
+// Licensed under the MIT License
+
+const path = require('path');
+
+module.exports = {
+  ci: {
+    upload: {
+      target: 'temporary-public-storage',
+    },
+  },
+};
