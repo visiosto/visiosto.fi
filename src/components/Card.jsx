e5b0a5fd5c8d064088be63dc9741d44ba4562f03
Antti Kivi <antti.kivi@visiosto.fi>
2021-01-31 16:20:52 +0200

    Fix the hover animation duration of the cards

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -10,7 +10,7 @@ export default (props) => {
     padding: 2rem;
     box-shadow: 0px 3px 1px rgba(0, 0, 0, 0.04), 0px 1px 1px rgba(0, 0, 0, 0.08),
       0px 3px 8px rgba(0, 0, 0, 0.08), 0px 15px 22px rgba(0, 0, 0, 0.06);
-    transition: box-shadow 50ms ease-in;
+    transition: box-shadow 100ms ease-in;
 
     &:hover {
       box-shadow: 0px 13px 11px rgba(0, 0, 0, 0.04), 0px 11px 11px rgba(0, 0, 0, 0.08),
