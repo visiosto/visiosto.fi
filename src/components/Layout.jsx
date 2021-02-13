b36a8c553fba787358c74a6704eae0a344e8a7fa
Antti Kivi <antti.kivi@visiosto.fi>
2021-02-13 20:17:14 +0200

    Export the global style to its own component

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -5,54 +5,11 @@ import React from 'react';
 import styled, { createGlobalStyle } from 'styled-components';
 
 import Footer from './Footer';
+import GlobalStyle from './GlobalStyle';
 import Head from './Head';
 import Header from './Header';
 
 import listenColorScheme from '../utils/listenColorScheme';
-
-const GlobalStyle = createGlobalStyle`
-  *, *::before, *::after {
-    box-sizing: inherit;
-  }
-
-  html {
-    box-sizing: border-box;
-  }
-
-  body {
-    background: var(--color-background);
-    font-family: ${(props) => props.theme.fonts.main};
-    font-size: 1rem;
-    font-weight: 400;
-    font-smoothing: antialiased;
-    line-height: 1.5;
-    color: var(--color-text);
-  }
-
-  h1, h2 {
-    clear: both;
-    font-family: ${(props) => props.theme.fonts.heading};
-    font-weight: 700;
-  }
-
-  h3, h4, h5, h6 {
-    clear: both;
-    font-family: ${(props) => props.theme.fonts.main};
-    font-weight: 700;
-  }
-
-  a {
-    color: var(--color-link);
-
-    &:visited {
-      color: var(--color-link);
-    }
-
-    &:hover, &:focus, &:active {
-      color: var(--color-link-hover);
-    }
-  }
-`;
 
 export default (props) => {
   listenColorScheme();
