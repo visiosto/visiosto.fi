8abfe41fad2bf24479a93e987f5ae9bba0acbddc
Antti Kivi <antti.kivi@visiosto.fi>
2021-02-15 21:11:34 +0200

    Add support for language switching links in the blog posts

--- src/components/createLanguageLink.jsx
+++ src/components/createLanguageLink.jsx
@@ -4,25 +4,51 @@
 import React from 'react';
 import { Link } from 'gatsby';
 
+import blogSlugs from '../../data/blog-slugs.json';
 import pageSlugs from '../data/page-slugs.json';
 import { DEFAULT_LANGUAGE } from '../constants';
 
-const createLanguageLink = (pageKey) => {
-  const slugs = pageSlugs;
+const pageKeySlashIndex = 1;
 
+const createLocalizedSlug = (toLang, slug) => {
+  let localized = toLang === DEFAULT_LANGUAGE ? '/' : `/${toLang}`;
+
+  if (slug in pageSlugs && toLang in pageSlugs[slug]) {
+    localized = `/${toLang}/${pageSlugs[slug][toLang]}`;
+
+    if (toLang === DEFAULT_LANGUAGE) {
+      localized = `/${pageSlugs[slug][toLang]}`;
+    }
+  }
+
+  return localized;
+};
+
+const createLanguageLink = (pageKey) => {
   return (linkProps) => {
     const toLang = linkProps.to;
-    let to = toLang === DEFAULT_LANGUAGE ? '/' : `/${toLang}`;
+    if (pageKey.startsWith('/blog/')) {
+      const page = createLocalizedSlug(toLang, 'blog');
 
-    if (pageKey in slugs && toLang in slugs[pageKey]) {
-      to = `/${toLang}/${slugs[pageKey][toLang]}`;
+      const blogPostFilenameRegex = /([0-9]+)\/([0-9]+)\/([0-9]+)\/(.+)/;
 
-      if (toLang === DEFAULT_LANGUAGE) {
-        to = `/${slugs[pageKey][toLang]}`;
-      }
+      // Blog posts don't have embedded permalinks.
+      // Their slugs follow a pattern: /blog/<year>/<month>/<day>/<slug>
+      // The date portion comes from the file name: <date>-<title>.md
+      const match = blogPostFilenameRegex.exec(pageKey.substring(pageKeySlashIndex));
+      const year = match[1];
+      const month = match[2];
+      const day = match[3];
+      const filename = match[4];
+
+      const date = `${year}-${month}-${day}`;
+
+      const linkPath = `${page}/${year}/${month}/${day}/${blogSlugs[date][filename][toLang]}`;
+
+      return <Link {...linkProps} to={linkPath} />;
+    } else {
+      return <Link {...linkProps} to={createLocalizedSlug(toLang, pageKey)} />;
     }
-
-    return <Link {...linkProps} to={to} />;
   };
 };
 
