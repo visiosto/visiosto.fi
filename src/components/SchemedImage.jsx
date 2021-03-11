da5263de8f26ee9f7b201d7aa52fa10aae76c4a7
Antti Kivi <antti.kivi@visiosto.fi>
2021-03-11 09:55:54 +0200

    feat: add the alt attribute to schemed images

--- src/components/SchemedImage.jsx
+++ src/components/SchemedImage.jsx
@@ -2,15 +2,38 @@
 // Licensed under the MIT License
 
 import React, { useContext } from 'react';
+import PropTypes from 'prop-types';
 import { GatsbyImage } from 'gatsby-plugin-image';
 
 import ThemeContext from './ThemeContext';
 
-export default (props) => {
+const propTypes = {
+  light: PropTypes.object.isRequired,
+  dark: PropTypes.object.isRequired,
+  alt: PropTypes.string,
+};
+
+const defaultProps = {
+  alt: '',
+};
+
+const SchemedImage = (props) => {
   const { colorMode } = useContext(ThemeContext);
 
-  const LightImage = () => <GatsbyImage image={props.light} {...props} />;
-  const DarkImage = () => <GatsbyImage image={props.dark} {...props} />;
+  const createImage = (image) => () =>
+    props.alt ? (
+      <GatsbyImage image={image} alt={props.alt} {...props} />
+    ) : (
+      <GatsbyImage image={props.light} alt="" role="presentation" {...props} />
+    );
+
+  const LightImage = createImage(props.light);
+  const DarkImage = createImage(props.dark);
 
   return colorMode === 'dark' ? <DarkImage /> : <LightImage />;
 };
+
+SchemedImage.propTypes = propTypes;
+SchemedImage.defaultProps = defaultProps;
+
+export default SchemedImage;
