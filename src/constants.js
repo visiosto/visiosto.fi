29427b64b575b5d588ce114c7abf6d65a458e5c0
Antti Kivi <antti.kivi@visiosto.fi>
2021-04-04 10:59:31 +0300

    refactor: remove unused constants

--- src/constants.js
+++ src/constants.js
@@ -4,10 +4,6 @@
 export const COLOR_MODE_KEY = 'color-mode';
 export const INITIAL_COLOR_MODE_CSS_PROP = '--initial-color-mode';
 
-export const AUTHOR_SLUG = 'author';
-export const BLOG_SLUG = 'blog';
-export const CATEGORY_SLUG = 'category';
-
 export const COOKIES_ACCEPTED_COOKIE_NAME = 'visiosto_cookies_accepted';
 export const DISABLE_ANALYTICS_COOKIE_NAME = 'visiosto_analytics_opt_out';
 
