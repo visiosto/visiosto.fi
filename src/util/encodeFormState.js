d3bcbc1dadb85cdfc95b51dbbbdd5b7e2cace31c
Antti Kivi <antti.kivi@visiosto.fi>
2021-04-04 19:32:43 +0300

    style: fix lint error

--- src/util/encodeFormState.js
+++ src/util/encodeFormState.js
@@ -3,7 +3,7 @@
 
 const encodeFormState = (data) =>
   Object.keys(data)
-    .map((key) => encodeURIComponent(key) + '=' + encodeURIComponent(data[key]))
+    .map((key) => `${encodeURIComponent(key)}=${encodeURIComponent(data[key])}`)
     .join('&');
 
 export default encodeFormState;
