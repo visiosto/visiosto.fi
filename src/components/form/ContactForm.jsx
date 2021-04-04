48379f3aea5fabe623d2dbebb49e3d487b7d7a0f
Antti Kivi <antti.kivi@visiosto.fi>
2021-04-04 16:19:07 +0300

    refactor: use a common component for the basic form div

--- src/components/form/ContactForm.jsx
+++ src/components/form/ContactForm.jsx
@@ -6,6 +6,8 @@ import styled from 'styled-components';
 import { PaperAirplaneIcon } from '@primer/octicons-react';
 import { injectIntl } from 'react-intl';
 
+import FormDiv from './FormDiv';
+
 import {
   CONTACT_FORM_NAME,
   FORM_POST_STATUS_ERROR,
@@ -17,87 +19,6 @@ import createIntl from '../../util/createIntl';
 
 const FormContainer = styled.div`
   text-align: center;
-`;
-
-const FormDiv = styled.div`
-  margin: 2em ${(props) => props.theme.layout.marginMobile};
-
-  @media screen and (${(props) => props.theme.devices.mobileL}) {
-    margin: 2em ${(props) => props.theme.layout.marginTablet};
-  }
-
-  @media screen and (${(props) => props.theme.devices.tablet}) {
-    margin: 2em ${(props) => props.theme.layout.marginTablet};
-  }
-
-  input {
-    text-align: center;
-  }
-
-  textarea {
-    @media screen and (${(props) => props.theme.devices.tablet}) {
-      min-width: 20rem;
-      min-height: 10rem;
-    }
-  }
-
-  input[type='email'],
-  input[type='tel'],
-  input[type='text'],
-  textarea {
-    margin: 0.6em 0;
-    border: none;
-    border-radius: 0.25rem;
-    padding: 0.4rem 1rem;
-    box-shadow: var(--color-box-shadow);
-    transition: box-shadow 75ms ease-in;
-    background: var(--color-background);
-    color: var(--color-text);
-
-    &:focus {
-      box-shadow: var(--color-box-shadow-hover);
-      outline: none;
-    }
-  }
-
-  input[type='radio'] {
-    margin: 0 0.5rem;
-  }
-
-  input[type='submit'],
-  button[type='submit'] {
-    display: inline-block;
-    border-style: none;
-    border-radius: 3rem;
-    border: 3px solid transparent;
-    padding: 1rem 1.5rem;
-    cursor: pointer;
-    box-shadow: none;
-    transition: all 100ms ease-in;
-    background-color: var(--color-link);
-    background-clip: padding-box;
-    font-weight: 400;
-    text-align: center;
-    text-decoration: none;
-    color: var(--color-text-button);
-
-    &:hover {
-      background-color: var(--color-link-hover);
-    }
-  }
-
-  label {
-    display: block;
-    margin: 1rem 0 0;
-  }
-
-  input.medium {
-    min-width: 20vw;
-  }
-
-  input.wider {
-    min-width: 30vw;
-  }
 `;
 
 const ButtonDiv = styled.div``;
--- src/components/form/RegisterForm.jsx
+++ src/components/form/RegisterForm.jsx
@@ -7,6 +7,7 @@ import { ArrowLeftIcon, ArrowRightIcon, PaperAirplaneIcon } from '@primer/octico
 import { injectIntl } from 'react-intl';
 
 import Button from '../Button';
+import FormDiv from './FormDiv';
 import LocalizedLink from '../link/LocalizedLink';
 
 import createIntl from '../../util/createIntl';
@@ -16,80 +17,6 @@ const FormContainer = styled.div`
 `;
 
 const FormPage = styled.div``;
-
-const FormDiv = styled.div`
-  margin: 2em ${(props) => props.theme.layout.marginMobile};
-
-  @media screen and (${(props) => props.theme.devices.mobileL}) {
-    margin: 2em ${(props) => props.theme.layout.marginTablet};
-  }
-
-  @media screen and (${(props) => props.theme.devices.tablet}) {
-    margin: 2em ${(props) => props.theme.layout.marginTablet};
-  }
-
-  input {
-    text-align: center;
-  }
-
-  input[type='email'],
-  input[type='tel'],
-  input[type='text'],
-  textarea {
-    margin: 0.6em 0;
-    border: none;
-    border-radius: 0.25rem;
-    padding: 0.4rem 1rem;
-    box-shadow: var(--color-box-shadow);
-    transition: box-shadow 75ms ease-in;
-    background: var(--color-background);
-    color: var(--color-text);
-
-    &:focus {
-      box-shadow: var(--color-box-shadow-hover);
-      outline: none;
-    }
-  }
-
-  input[type='radio'] {
-    margin: 0 0.5rem;
-  }
-
-  input[type='submit'],
-  button[type='submit'] {
-    display: inline-block;
-    border-style: none;
-    border-radius: 3rem;
-    border: 3px solid transparent;
-    padding: 1rem 1.5rem;
-    cursor: pointer;
-    box-shadow: none;
-    transition: all 100ms ease-in;
-    background-color: var(--color-link);
-    background-clip: padding-box;
-    font-weight: 400;
-    text-align: center;
-    text-decoration: none;
-    color: var(--color-text-button);
-
-    &:hover {
-      background-color: var(--color-link-hover);
-    }
-  }
-
-  label {
-    display: block;
-    margin: 1rem 0 0;
-  }
-
-  input.medium {
-    min-width: 20vw;
-  }
-
-  input.wider {
-    min-width: 30vw;
-  }
-`;
 
 const SwitchInputSpan = styled.span`
   display: inline-block;
