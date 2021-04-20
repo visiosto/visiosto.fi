8821b1d1aa726ab09224766d1fed0c4a8bbd3608
Antti Kivi <antti.kivi@visiosto.fi>
2021-04-21 01:24:27 +0300

    build: configure ESLint for TypeScript

---
+++ .eslintrc.js
@@ -0,0 +1,49 @@
+// Copyright (c) 2021 Visiosto oy
+// Licensed under the MIT License
+
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'airbnb',
+    'airbnb-typescript',
+    'airbnb/hooks',
+    'plugin:prettier/recommended',
+    'prettier',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    project: './tsconfig.json',
+    sourceType: 'module',
+  },
+  plugins: ['react', 'prettier', '@typescript-eslint'],
+  settings: {
+    react: {
+      version: 'detect',
+    },
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      },
+    },
+  },
+  rules: {
+    'import/extensions': [
+      'error',
+      'always',
+      { js: 'never', jsx: 'never', ts: 'never', tsx: 'never' },
+    ],
+    'import/no-extraneous-dependencies': ['error', { devDependencies: true }],
+    'no-sync': 'off',
+    'no-use-before-define': 'off',
+    'prettier/prettier': 'error',
+    'react/display-name': 'off',
+    'react/jsx-filename-extension': ['error', { extensions: ['.jsx', '.tsx'] }],
+  },
+};
