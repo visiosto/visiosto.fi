3dc46f998d128eb0a95df779c15b2e6587a410ac
Antti Kivi <antti.kivi@visiosto.fi>
2021-05-09 21:01:11 +0300

    fix: use the correct Theme component in the tests

--- test/renderWithProviders.js
+++ test/renderWithProviders.js
@@ -4,9 +4,11 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import { IntlProvider } from 'react-intl';
+import { Theme } from '@visiosto/components';
 
 import App from '../src/components/App';
-import Theme from '../src/components/Theme';
+
+import theme from '../src/theme';
 
 export default function renderWithProviders(component, locale) {
   // eslint-disable-next-line global-require, no-undef, import/no-dynamic-require
@@ -16,9 +18,8 @@ export default function renderWithProviders(component, locale) {
     // eslint-disable-next-line react/jsx-filename-extension
     <App>
       <IntlProvider locale={locale} messages={messages}>
-        <Theme>{component}</Theme>
+        <Theme theme={theme}>{component}</Theme>
       </IntlProvider>
-      ,
     </App>,
   );
 }
