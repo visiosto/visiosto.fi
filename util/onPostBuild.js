4f82db32a4f3f388aa591514b1dca90173764535
Antti Kivi <antti.kivi@visiosto.fi>
2021-07-17 17:43:30 +0300

    fix: fix the wrong name of the variable in the search data for portfolio

--- util/onPostBuild.js
+++ util/onPostBuild.js
@@ -566,7 +566,7 @@ module.exports = async function onPostBuild({ graphql, reporter }) {
   const { portfolioPaths } = query.data;
 
   query.data.portfolioReferences.edges.forEach(({ node: referenceNode }) => {
-    const { description, node_locale: locale, contentful_id: id, name, slug } = referenceNode;
+    const { description, node_locale: locale, contentful_id: id, name: title, slug } = referenceNode;
     const { excerpt } = description.childMarkdownRemark;
     reporter.verbose(
       `Creating search index entry for the portfolio reference '${slug}' (locale: ${locale})`,
@@ -582,7 +582,7 @@ module.exports = async function onPostBuild({ graphql, reporter }) {
     const page = {
       slug: pagePath,
       id,
-      name,
+      title,
       excerpt,
       content: createPortfolioReferenceContent(referenceNode),
     };
