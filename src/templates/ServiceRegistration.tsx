f66b0f7fd83a4bce685ce12d4dd7536bc17b670b
Antti Kivi <antti.kivi@visiosto.fi>
2021-08-11 18:20:59 +0300

    feat: add placeholder for the service registration page

---
+++ src/templates/ServiceRegistration.tsx
@@ -0,0 +1,135 @@
+// Copyright (c) 2021 Visiosto oy
+// Licensed under the MIT License
+
+import React from 'react';
+import PropTypes from 'prop-types';
+import { graphql } from 'gatsby';
+import styled from 'styled-components';
+import { useIntl } from 'react-intl';
+
+import Intl from '../components/Intl';
+import Layout from '../components/layout/Layout';
+import Rule from '../components/Rule';
+import Theme from '../components/Theme';
+
+import createInternationalization from '../util/createInternationalization';
+
+const Div = styled.div`
+  margin: 2em ${(props) => props.theme.layout.marginMobile};
+
+  .centered {
+    text-align: center;
+  }
+
+  @media screen and (${(props) => props.theme.devices.mobileL}) {
+    margin: 3em ${(props) => props.theme.layout.marginTablet};
+  }
+
+  @media screen and (${(props) => props.theme.devices.tablet}) {
+    margin: 3em ${(props) => props.theme.layout.marginDesktop};
+  }
+`;
+
+const Separator = styled.div`
+  display: flex;
+  justify-content: center;
+  margin: 3em 0;
+
+  @media screen and (${(props) => props.theme.devices.mobileL}) {
+    margin: 4em 0;
+  }
+
+  @media screen and (${(props) => props.theme.devices.tablet}) {
+    margin: 5em 0;
+  }
+`;
+
+const H2 = styled.h2`
+  margin: 2em ${(props) => props.theme.layout.marginMobile};
+  font-size: 2.2rem;
+  text-align: center;
+
+  @media screen and (${(props) => props.theme.devices.mobileL}) {
+    margin: 3em ${(props) => props.theme.layout.marginTablet};
+  }
+
+  @media screen and (${(props) => props.theme.devices.tablet}) {
+    margin: 3em ${(props) => props.theme.layout.marginTablet};
+  }
+`;
+
+const propTypes = {
+  // eslint-disable-next-line react/forbid-prop-types
+  data: PropTypes.object.isRequired,
+  pageContext: PropTypes.shape({
+    clientType: PropTypes.string,
+    locale: PropTypes.string,
+    pageID: PropTypes.string,
+  }).isRequired,
+};
+
+function Page({ data, pageContext }) {
+  const intl = createInternationalization(useIntl());
+
+  const { contentfulPage: page } = data;
+  const { locale, pageID } = pageContext;
+
+  return (
+    <Layout locale={locale} pageID={pageID} title={page.title}>
+      <Div dangerouslySetInnerHTML={{ __html: page.body.childMarkdownRemark.html }} />
+      <Separator>
+        <Rule color="peach" mode={1} ignoreColorScheme />
+      </Separator>
+      <H2>Placeholder</H2>
+    </Layout>
+  );
+}
+
+Page.propTypes = propTypes;
+
+function ServiceRegistration({ data, pageContext }) {
+  const { simpleLocales } = data.site.siteMetadata;
+  const { locale } = pageContext;
+  return (
+    <Intl locale={simpleLocales[locale.replace('-', '_')]}>
+      <Theme>
+        <Page data={data} pageContext={pageContext} />
+      </Theme>
+    </Intl>
+  );
+}
+
+ServiceRegistration.propTypes = propTypes;
+
+export default ServiceRegistration;
+
+export const pageQuery = graphql`
+  query ServiceRegistrationQuery($pageID: String, $locale: String) {
+    site {
+      siteMetadata {
+        simpleLocales {
+          en_GB
+          fi
+        }
+      }
+    }
+    contentfulPage(contentful_id: { eq: $pageID }, node_locale: { eq: $locale }) {
+      title
+      body {
+        childMarkdownRemark {
+          html
+        }
+      }
+      description {
+        description
+      }
+      image {
+        description
+        file {
+          contentType
+          url
+        }
+      }
+    }
+  }
+`;
